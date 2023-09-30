216fa5a80eeb33a439b36cb2ad499faef516f520
Victor Galbraith <VictorG87@Gmail.com>
2023-09-30 22:52:04 +0000

    added User Registration

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,14 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.service.AccountService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +16,19 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
 
+    private final AccountService accountService;
+
+    @Autowired
+    public SocialMediaController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping("/register")
+	public ResponseEntity<Account> addAccount(@RequestBody Account account) throws Exception {
+        Account addedAccount = accountService.persistAccount(account);
+        return new ResponseEntity<Account> (addedAccount, HttpStatus.OK);
+	}
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,44 @@
 package com.example.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.exception.ConflictException;
+import com.example.exception.BadRequestException;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+    AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    /**
+     * Given a brand new transient account (meaning, no such account exists yet in the database),
+     * persist the account to the database (create a new database record for the account entity.)
+     */
+    public Account persistAccount(Account account) throws Exception {
+    	if (account.getUsername().equals("")) {
+    		throw new BadRequestException("Username cannot be blank.");
+        }
+    	
+        if (account.getPassword().length() < 4) {
+            throw new BadRequestException("Password must be at least 4 characters long.");
+        }
+    
+        List<Account> accounts = accountRepository.findAll();
+        for (Account a : accounts) {
+            if (a.getUsername().equals(account.getUsername())) {
+                throw new ConflictException("Username already exists.");
+            }
+        }
+        return accountRepository.save(account);
+    }
 }
